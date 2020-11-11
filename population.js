03fe379becc39c6782ce248d319dcaad7949065a
Abeer <abeer.technology@gmail.com>
2020-11-11 14:38:48 +0600

    Added { useUnifiedTopology: true } at line 3

--- population.js
+++ population.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/playground')
+mongoose.connect('mongodb://localhost/playground', { useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
